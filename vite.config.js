1d183b08d735bbd9dbb6b8dcc64251f216c86a57
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2024-12-03 00:27:26 +0530

    update viteconfig to output css with a predictable name

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,12 @@ export default defineConfig({
       input: 'src/content.jsx',
       output: {
         entryFileNames: 'content.js',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'styles.css'; // Rename CSS files predictably
+          }
+          return 'assets/[name][extname]'; // Default naming for other assets
+        },
         dir: 'dist',
       },
     },
