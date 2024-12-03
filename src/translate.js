20172e5710d6f5b4441d9ef6304582d4b9773c65
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2024-12-03 12:44:39 +0530

    fix formatting using prettier

--- src/translate.js
+++ src/translate.js
@@ -1,30 +1,33 @@
-const initTranslator = async (srcLang, tgtLang ) => {
-    const languagePair = { sourceLanguage: srcLang, targetLanguage: tgtLang };
-    // eslint-disable-next-line no-undef
-    const canTranslate = await translation.canTranslate(languagePair);
-    let translator;
-    if (canTranslate !== 'no') {
-      if (canTranslate === 'readily') {
-        // eslint-disable-next-line no-undef
-        translator = await translation.createTranslator(languagePair);
-        return { translator, status: 'Ready' };
-
-      } else {
-        return { translator, status: 'Downloading' };
-      } 
+const initTranslator = async (srcLang, tgtLang) => {
+  const languagePair = { sourceLanguage: srcLang, targetLanguage: tgtLang };
+  // eslint-disable-next-line no-undef
+  const canTranslate = await translation.canTranslate(languagePair);
+  let translator;
+  if (canTranslate !== 'no') {
+    if (canTranslate === 'readily') {
+      // eslint-disable-next-line no-undef
+      translator = await translation.createTranslator(languagePair);
+      return { translator, status: 'Ready' };
     } else {
-        throw new Error('Translation is not supported on this device.');
+      return { translator, status: 'Downloading' };
     }
-    
+  } else {
+    throw new Error('Translation is not supported on this device.');
+  }
 };
 
- export const translateText = async (text, srcLang, tgtLang) => {
-  console.log(" here is the source language", srcLang, "and here is the target language", tgtLang);
-    const { translator, status } = await initTranslator(srcLang, tgtLang);
-    if (status === 'Downloading') {
-        return 'Model is currently downloading.';
-    }
-    const translatedText = await translator.translate(text);
-    console.log('Translated text:', translatedText);
-    return translatedText;
-}
+export const translateText = async (text, srcLang, tgtLang) => {
+  console.log(
+    ' here is the source language',
+    srcLang,
+    'and here is the target language',
+    tgtLang
+  );
+  const { translator, status } = await initTranslator(srcLang, tgtLang);
+  if (status === 'Downloading') {
+    return 'Model is currently downloading.';
+  }
+  const translatedText = await translator.translate(text);
+  console.log('Translated text:', translatedText);
+  return translatedText;
+};
