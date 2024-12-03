1459a66e1cb240bb0ff56c35237d22d3a62aea25
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2024-12-03 12:43:32 +0530

    fix formatting using prettier

--- src/detect-language.js
+++ src/detect-language.js
@@ -1,29 +1,28 @@
 const initDetector = async () => {
-    // eslint-disable-next-line no-undef
-    const canDetect = await translation.canDetect();
-    let detector;
-    if (canDetect !== 'no') {
-        if (canDetect === 'readily') {
-            // eslint-disable-next-line no-undef
-            detector = await translation.createDetector();
-            return { detector, status: 'Ready' };
-        } else {
-            return { detector, status: 'Downloading' };
-        }
+  // eslint-disable-next-line no-undef
+  const canDetect = await translation.canDetect();
+  let detector;
+  if (canDetect !== 'no') {
+    if (canDetect === 'readily') {
+      // eslint-disable-next-line no-undef
+      detector = await translation.createDetector();
+      return { detector, status: 'Ready' };
     } else {
-        throw new Error('Language detection is not supported on this device.');
+      return { detector, status: 'Downloading' };
     }
+  } else {
+    throw new Error('Language detection is not supported on this device.');
+  }
 };
 
 export const detectLanguage = async (text) => {
-
-    const { detector, status } = await initDetector();
-    if (status === 'Downloading') {
-        return 'Model is currently downloading.'
-    }
-    console.log('Detecting language for:', text);
-    const results = await detector.detect(text);
-    const detectedLanguage = results[0].detectedLanguage;
-    console.log('Detected language:', detectedLanguage);
-    return detectedLanguage;
+  const { detector, status } = await initDetector();
+  if (status === 'Downloading') {
+    return 'Model is currently downloading.';
+  }
+  console.log('Detecting language for:', text);
+  const results = await detector.detect(text);
+  const detectedLanguage = results[0].detectedLanguage;
+  console.log('Detected language:', detectedLanguage);
+  return detectedLanguage;
 };
