528e4dbab4f10f2a88d5f5e2bbfadd810a537c45
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2024-12-04 03:49:42 +0530

    implement different modes to render options

--- src/components/AiOptions.jsx
+++ src/components/AiOptions.jsx
@@ -1,6 +1,10 @@
 import PropTypes from 'prop-types';
 import { MdOutlineSummarize } from 'react-icons/md';
 import { AiOutlineRobot } from 'react-icons/ai';
+import { FaVolumeUp } from 'react-icons/fa';
+
+
+
 import { RiBrushLine } from 'react-icons/ri';
 import { RiEdit2Line } from 'react-icons/ri';
 import { MdTranslate } from 'react-icons/md';
@@ -8,44 +12,77 @@ import { FaTable } from 'react-icons/fa';
 import { BsPencilSquare } from 'react-icons/bs';
 import '@fontsource/orbitron';
 
-const AiOptions = ({ onOptionSelect }) => {
-  const aiOptionsList = [
-    {
-      label: 'Summarize',
-      icon: <MdOutlineSummarize />,
-      onClick: () => onOptionSelect('summarize'),
-    },
-    {
-      label: 'Polish & Perfect',
-      icon: <RiBrushLine />,
-      onClick: () => onOptionSelect('write_better'),
-    },
-    {
-      label: 'Ask AI',
-      icon: <AiOutlineRobot />,
-      onClick: () => onOptionSelect('ask_ai'),
-    },
-    {
-      label: 'AI Smart Tables',
-      icon: <FaTable />,
-      onClick: () => onOptionSelect('ai_table'),
-    },
-    {
-      label: 'Translate',
-      icon: <MdTranslate />,
-      onClick: () => onOptionSelect('translate'),
-    },
-    {
-      label: 'Write using AI',
-      icon: <BsPencilSquare />,
-      onClick: () => onOptionSelect('write'),
-    },
-    {
-      label: 'Rewrite using AI',
-      icon: <RiEdit2Line />,
-      onClick: () => onOptionSelect('rewrite'),
-    },
-  ];
+const AiOptions = ({ onOptionSelect, mode }) => {
+  let aiOptionsList = [];
+  if (mode == 'standalone') {
+    aiOptionsList = [
+      {
+        label: 'Summarize',
+        icon: <MdOutlineSummarize />,
+        onClick: () => onOptionSelect('summarize'),
+      },
+      {
+        label: 'AI Smart Tables',
+        icon: <FaTable />,
+        onClick: () => onOptionSelect('ai_table'),
+      },
+      {
+        label: 'Chat with AI',
+        icon: <AiOutlineRobot />,
+        onClick: () => onOptionSelect('ask_ai'),
+      },
+      {
+        label: 'Write using AI',
+        icon: <BsPencilSquare />,
+        onClick: () => onOptionSelect('write'),
+      },
+      {
+        label: 'Vocal Mode',
+        icon: <FaVolumeUp />,
+        onClick: () => onOptionSelect('ask_ai'),
+      },
+    ];
+  } else {
+    aiOptionsList = [
+      {
+        label: 'Summarize',
+        icon: <MdOutlineSummarize />,
+        onClick: () => {
+          console.log('summarize');
+          onOptionSelect('summarize') }
+      },
+      {
+        label: 'Polish & Perfect',
+        icon: <RiBrushLine />,
+        onClick: () => onOptionSelect('write_better'),
+      },
+      {
+        label: 'Ask AI',
+        icon: <AiOutlineRobot />,
+        onClick: () => onOptionSelect('ask_ai'),
+      },
+      {
+        label: 'AI Smart Tables',
+        icon: <FaTable />,
+        onClick: () => onOptionSelect('ai_table'),
+      },
+      {
+        label: 'Translate',
+        icon: <MdTranslate />,
+        onClick: () => onOptionSelect('translate'),
+      },
+      {
+        label: 'Write using AI',
+        icon: <BsPencilSquare />,
+        onClick: () => onOptionSelect('write'),
+      },
+      {
+        label: 'Rewrite using AI',
+        icon: <RiEdit2Line />,
+        onClick: () => onOptionSelect('rewrite'),
+      },
+    ];
+  }
 
   const containerStyle = {
     backgroundColor: '#1c1c1e',
@@ -107,5 +144,7 @@ const AiOptions = ({ onOptionSelect }) => {
 AiOptions.propTypes = {
   onOptionSelect: PropTypes.func.isRequired,
 };
-
+AiOptions.propTypes = {
+  mode: PropTypes.string.isRequired,
+};
 export default AiOptions;
